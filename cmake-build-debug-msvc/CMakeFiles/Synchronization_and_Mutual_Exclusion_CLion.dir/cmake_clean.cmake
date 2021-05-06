9e1b5581c3e089dd3d5cbd6c2c4e7190629e00e2
wellexam <el.zhao.me@gmail.com>
2021-05-06 23:58:53 +0800

    writer first done

---
+++ cmake-build-debug-msvc/CMakeFiles/Synchronization_and_Mutual_Exclusion_CLion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Synchronization_and_Mutual_Exclusion_CLion.dir/reader_writer.cpp.obj"
+  "Synchronization_and_Mutual_Exclusion_CLion.exe"
+  "Synchronization_and_Mutual_Exclusion_CLion.exe.manifest"
+  "Synchronization_and_Mutual_Exclusion_CLion.lib"
+  "Synchronization_and_Mutual_Exclusion_CLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Synchronization_and_Mutual_Exclusion_CLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
